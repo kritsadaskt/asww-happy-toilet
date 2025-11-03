4c77af31256e9ee138dd46709c80d6d9b4c1d623
Kritsada S. <k.sangkhatad@gmail.com>
2025-11-03 10:48:23 +0700

    remove static export

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  basePath: '/happy-toilet-test',
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
