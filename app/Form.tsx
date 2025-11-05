97c457c06a93dcd335ec1e07737c204c517001ef
kritsadaskt <k.sangkhatad@hotmail.com>
2025-11-05 16:04:40 +0700

    edit form placeholder

--- app/Form.tsx
+++ app/Form.tsx
@@ -130,7 +130,7 @@ export default function Form() {
                   id="workTitle"
                   name="workTitle"
                   className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
-                  placeholder="หัวข้องาน"
+                  placeholder="ชื่อผลงาน"
                 />
               </div>
 
