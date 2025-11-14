48c8042c67c7b5868fd5a14b0b3090de43ff0f39
kritsadaskt <k.sangkhatad@hotmail.com>
2025-11-14 15:54:40 +0700

    add og metadat

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,31 @@ const ibmPlexSansThai = IBM_Plex_Sans_Thai({
 });
 
 export const metadata: Metadata = {
-  title: "The AssetWise Happy Toilet Project",
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || "https://thehappytoiletproject.com"),
+  title: "The Happy Toilet Project",
   description: "The AssetWise Happy Toilet Project",
+  openGraph: {
+    title: "The Happy Toilet Project",
+    description: "ชวนคุณออกแบบความสุข…ให้เกิดขึ้นจริง กับโครงการประกวดออกแบบห้องน้ำเพื่อสาธารณะ “สุขา สุขี : THE HAPPY TOILET” พร้อมโอกาสแปลงไอเดียเป็นงานสร้างจริง เงินรางวัลรวมมูลค่า 260,000 บาท",
+    url: "https://thehappytoiletproject.com/",
+    siteName: "The Happy Toilet Project",
+    images: [
+      {
+        url: "/happy_toilet_desktop_banner.png",
+        width: 1866,
+        height: 933,
+        alt: "The Happy Toilet Project",
+      },
+    ],
+    locale: "th_TH",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "The AssetWise Happy Toilet Project",
+    description: "ชวนคุณออกแบบความสุข…ให้เกิดขึ้นจริง กับโครงการประกวดออกแบบห้องน้ำเพื่อสาธารณะ “สุขา สุขี : THE HAPPY TOILET” พร้อมโอกาสแปลงไอเดียเป็นงานสร้างจริง เงินรางวัลรวมมูลค่า 260,000 บาท",
+    images: ["/happy_toilet_desktop_banner.png"],
+  },
 };
 
 export default function RootLayout({
